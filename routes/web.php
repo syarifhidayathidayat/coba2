2c8a2e17b63f68e335875700026459cda923564c
syarif <syarif64bit@gmail.com>
2025-06-13 01:33:36 +0800

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -21,21 +21,24 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('pegawai', PegawaiController::class);
+    Route::resource('penempatan', PenempatanController::class);
+    Route::resource('penyedia', PenyediaController::class)->parameters([
+        'penyedia' => 'penyedia'
+    ]);
+    Route::resource('sp', SpController::class);
+    Route::get('/sp/{id}', [SpController::class, 'show'])->name('sp.show');
+    Route::get('/barang/semua', [BarangController::class, 'indexSemuaBarang'])->name('barang.semua');
+    Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create');
+
+    Route::get('sp/{id}/barang/create', [App\Http\Controllers\BarangController::class, 'create'])->name('barang.create');
+    Route::post('sp/{id}/barang', [App\Http\Controllers\BarangController::class, 'store'])->name('barang.store');
     
 });
-Route::resource('pegawai', PegawaiController::class);
-Route::resource('penempatan', PenempatanController::class);
 
 
-Route::resource('penyedia', PenyediaController::class)->parameters([
-    'penyedia' => 'penyedia'
-]);
-Route::resource('sp', SpController::class);
-Route::get('/sp/{id}', [SpController::class, 'show'])->name('sp.show');
-Route::get('/barang/semua', [BarangController::class, 'indexSemuaBarang'])->name('barang.semua');
-Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create');
 
-Route::get('sp/{id}/barang/create', [App\Http\Controllers\BarangController::class, 'create'])->name('barang.create');
-Route::post('sp/{id}/barang', [App\Http\Controllers\BarangController::class, 'store'])->name('barang.store');
+
+
 
 require __DIR__.'/auth.php';
