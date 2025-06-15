600af9ecded7414fe136dde7d5a869c8420bfc8e
Syarif Hidayat <syarifhidayat@poltekkes-banjarmasin.ac.id>
2025-06-15 11:13:46 -0400

    add kwitansi

--- resources/views/bast/print/kwitansi.blade.php
+++ resources/views/bast/print/kwitansi.blade.php
@@ -24,7 +24,7 @@
             margin-bottom: 30px;
         }
         .content p {
-            margin: 10px 0;
+            margin: 8px 0;
         }
         table {
             width: 100%;
@@ -59,7 +59,7 @@
         }
         .info-label {
             display: inline-block;
-            width: 150px;
+            width: 170px;
         }
         .info-value {
             display: inline-block;
@@ -77,7 +77,7 @@
 <body>
     <div class="header">
         <div class="info-item">
-            <span class="info-label">A</span>
+            <span class="info-label">TA</span>
             <span class="info-value">: {{ date('Y') }}</span>
         </div>
         <div class="info-item">
@@ -86,7 +86,7 @@
         </div>
         <div class="info-item">
             <span class="info-label">Mata Anggaran</span>
-            <span class="info-value">: 5034.EBB.951.055.A.532111</span>
+            <span class="info-value">: {{ $bast->sp->akun ?? '-' }}</span>
         </div>
     </div>
 
@@ -102,11 +102,11 @@
             </div>
             <div class="info-item">
                 <span class="info-label">Jumlah Uang</span>
-                <span class="info-value">: Rp {{ number_format($bast->sp->total_kontrak - ($bast->sp->total_kontrak * 0.015), 0, ',', '.') }}</span>
+                <span class="info-value">: Rp {{ number_format($bast->sp->total_kontrak , 0, ',', '.') }}</span>
             </div>
             <div class="info-item">
                 <span class="info-label">Terbilang</span>
-                <span class="info-value">: {{ ucwords(\App\Helpers\Terbilang::make($bast->sp->total_kontrak - ($bast->sp->total_kontrak * 0.015))) }} Rupiah</span>
+                <span class="info-value">: {{ ucwords(\App\Helpers\Terbilang::make($bast->sp->total_kontrak )) }} Rupiah</span>
             </div>
             <div class="info-item">
                 <span class="info-label">Untuk Pembayaran</span>
@@ -119,7 +119,7 @@
                         <tr>
                             <th>No</th>
                             <th>Nama Barang</th>
-                            <th>Qty</th>
+                            <th>Vol</th>
                             <th>Harga Satuan</th>
                             <th>Ongkos Kirim</th>
                             <th>Jumlah Harga</th>
@@ -148,14 +148,7 @@
                 </div>
             @endif
 
-            <div class="info-item">
-                <span class="info-label">Metode Pengadaan</span>
-                <span class="info-value">: {{ $bast->sp->metode }}</span>
-            </div>
-            <div class="info-item">
-                <span class="info-label">Akun</span>
-                <span class="info-value">: {{ $bast->sp->akun }}</span>
-            </div>
+          
         @else
             <div class="error-message">
                 Data SP tidak tersedia. Silakan periksa kembali data BAST ini.
@@ -165,18 +158,21 @@
 
     <div class="footer">
         <div class="signature">
-            <p>Penerima,</p>
-            <br><br><br>
-            <p>( {{ $bast->sp->penyedia->nama_direktur_penyedia ?? '-' }} )</p>
-            <p>Direktur {{ $bast->sp->penyedia->nama_penyedia ?? '-' }}</p>
-        </div>
-        <div class="signature">
-            <p>Pembayar,</p>
+            <p>a.n Kuasa Pengguna Anggaran </p>
+            <p>Pejabat Pembuat Komitmen</p>
             <br><br><br>
             <p>( [Nama Pembayar] )</p>
-            <p>Pejabat Pembuat Komitmen</p>
-            <p>Poltekkes Kemenkes Banjarmasin</p>
+           
+            
         </div>
+        <div class="signature">
+            <br><br>
+            <p> {{ $bast->sp->penyedia->nama_penyedia ?? '-' }}</p>
+            <br><br><br>
+            <p> <b>{{ $bast->sp->penyedia->nama_direktur_penyedia ?? '-' }} </b> </p>
+            <p> Direktur</p>
+        </div>
+        
         <div class="clear"></div>
     </div>
 </body>
