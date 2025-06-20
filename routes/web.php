e2f56d2ae4a4a9c3c5f274a8146503781f68d0bc
syarif <syarif64bit@gmail.com>
2025-06-20 14:57:37 +0800

    ok

--- routes/web.php
+++ routes/web.php
@@ -23,59 +23,59 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('pegawai', PegawaiController::class);
-    Route::middleware(['role:admin|PPK'])->group(function () {
-        Route::resource('penempatan', PenempatanController::class);
-    });
-    Route::resource('penyedia', PenyediaController::class)->parameters([
-        'penyedia' => 'penyedia'
-    ]);
-
-
-
-    Route::middleware(['role:admin|Pejabat-Pengadaan52'])->group(function () {
-        Route::get('/sp/{id}', [SpController::class, 'show'])->name('sp.show');
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::resource('pegawai', PegawaiController::class);
+    
+        Route::middleware(['role:admin|PPK'])->group(function () {
+            Route::resource('penempatan', PenempatanController::class);
         });
-
-    Route::middleware(['role:admin|PPK|Pejabat-Pengadaan53'])->group(function () {
-    Route::resource('sp', SpController::class); 
-    Route::get('/barang/semua', [BarangController::class, 'indexSemuaBarang'])->name('barang.semua');
-    Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create');
-    Route::get('sp/{id}/barang/create', [App\Http\Controllers\BarangController::class, 'create'])->name('barang.create');
-    Route::post('sp/{id}/barang', [App\Http\Controllers\BarangController::class, 'store'])->name('barang.store');
+    
+        Route::resource('penyedia', PenyediaController::class)->parameters([
+            'penyedia' => 'penyedia'
+        ]);
+    
+        // 🚫 DULU DI DALAM MIDDLEWARE ROLE
+        // Route::middleware(['role:admin|PPK|Pejabat-Pengadaan53'])->group(function () {
+        //     Route::resource('sp', SpController::class); 
+    
+        // ✅ PINDAHKAN KE SINI UNTUK SEMENTARA
+        Route::resource('sp', SpController::class); 
+    
+        Route::get('/barang/semua', [BarangController::class, 'indexSemuaBarang'])->name('barang.semua');
+        Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create');
+        Route::get('sp/{id}/barang/create', [App\Http\Controllers\BarangController::class, 'create'])->name('barang.create');
+        Route::post('sp/{id}/barang', [App\Http\Controllers\BarangController::class, 'store'])->name('barang.store');
+    
+        Route::get('/sp/{id}', [SpController::class, 'show'])->name('sp.show');
+        Route::get('/sp/{id}/cetak', [SpController::class, 'cetak'])->name('sp.cetak');
+    
+        Route::resource('paket-pekerjaan', PaketPekerjaanController::class);
+    
+        // BAST
+        Route::get('/bast', [BastController::class, 'index'])->name('bast.index');
+        Route::get('/bast/create/{sp}', [BastController::class, 'create'])->name('bast.create');
+        Route::post('/bast', [BastController::class, 'store'])->name('bast.store');
+        Route::get('/bast/{bast}', [BastController::class, 'show'])->name('bast.show');
+        Route::get('/bast/{bast}/print/bast', [BastController::class, 'printBast'])->name('bast.print.bast');
+        Route::get('/bast/{bast}/print/bapem', [BastController::class, 'printBapem'])->name('bast.print.bapem');
+        Route::get('/bast/{bast}/print/bap', [BastController::class, 'printBap'])->name('bast.print.bap');
+        Route::get('/bast/{bast}/print/kwitansi', [BastController::class, 'printKwitansi'])->name('bast.print.kwitansi');
+    
+        Route::get('sp/{sp_id}/barang/{nama_barang}/edit', [App\Http\Controllers\BarangController::class, 'edit'])->name('barang.edit');
+        Route::put('sp/{sp_id}/barang/{nama_barang}', [App\Http\Controllers\BarangController::class, 'update'])->name('barang.update');
+    
+        Route::middleware(['auth', 'role:admin'])->group(function () {
+            Route::resource('menu', MenuController::class);
+            Route::resource('user', UserController::class);
+            Route::resource('institusi', App\Http\Controllers\InstitusiController::class);
+        });
+    
+        Route::get('/search', [SearchController::class, 'index'])->name('search');
     });
-
- 
-
-    Route::resource('paket-pekerjaan', PaketPekerjaanController::class);
-
-    // BAST Routes
-    Route::get('/bast', [BastController::class, 'index'])->name('bast.index');
-    Route::get('/bast/create/{sp}', [BastController::class, 'create'])->name('bast.create');
-    Route::post('/bast', [BastController::class, 'store'])->name('bast.store');
-    Route::get('/bast/{bast}', [BastController::class, 'show'])->name('bast.show');
-    Route::get('/bast/{bast}/print/bast', [BastController::class, 'printBast'])->name('bast.print.bast');
-    Route::get('/bast/{bast}/print/bapem', [BastController::class, 'printBapem'])->name('bast.print.bapem');
-    Route::get('/bast/{bast}/print/bap', [BastController::class, 'printBap'])->name('bast.print.bap');
-    Route::get('/bast/{bast}/print/kwitansi', [BastController::class, 'printKwitansi'])->name('bast.print.kwitansi');
-    Route::get('/sp/{id}/cetak', [SpController::class, 'cetak'])->name('sp.cetak');
-
-    Route::get('sp/{sp_id}/barang/{nama_barang}/edit', [App\Http\Controllers\BarangController::class, 'edit'])->name('barang.edit');
-    Route::put('sp/{sp_id}/barang/{nama_barang}', [App\Http\Controllers\BarangController::class, 'update'])->name('barang.update');
-
-    // Menu Management Routes
-    Route::middleware(['auth', 'role:admin'])->group(function () {
-        Route::resource('menu', MenuController::class);
-        Route::resource('user', UserController::class);
-        Route::resource('institusi', App\Http\Controllers\InstitusiController::class);
-    });
-
-    Route::get('/search', [SearchController::class, 'index'])->name('search');
-});
+    
 
 
 
