f407816e2289158c9bd4c6eee8b1960fef3654ef
syarif <syarif64bit@gmail.com>
2025-06-26 00:47:13 +0800

    ganti font poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                // poppins: ['Poppins', 'sans-serif'],
+                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
             },
         },
     },
