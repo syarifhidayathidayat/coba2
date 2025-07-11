e2771602afbdb301d9049239b70180c8bf94e3a3
syarif <syarif64bit@gmail.com>
2025-07-11 17:26:02 +0800

    fix resource after build

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="id">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ config('app.name', 'Pengadaan') }}</title>
 
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Pengadaan') }}</title>
+
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
 
-  @yield('content')
+    @yield('content')
 
 </body>
+
 </html>
